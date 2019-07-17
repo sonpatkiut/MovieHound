8cd2d0c469a5a8e893185925db4550211595e1db
sonpatkiut <sonpatkiut@gmail.com>
2019-07-17 07:36:37 +0530

    Update TrailersAdapter.java

--- RatingStar/app/src/main/java/com/example/icsssd358/ratingstar/TrailersAdapter.java
+++ RatingStar/app/src/main/java/com/example/icsssd358/ratingstar/TrailersAdapter.java
@@ -7,10 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -32,26 +29,12 @@ public class TrailersAdapter extends RecyclerView.Adapter<TrailersAdapter.MyView
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, final int i) {
         myViewHolder.name.setText(list.get(i).getName());
-        /*myViewHolder.date.setText("Date : " + list.get(i).getDate());
-        myViewHolder.ratings.setText(list.get(i).getRatings());
-        myViewHolder.ratings.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.staricon, 0);*/
 
-        /*Glide.with(context)
-                .load(MoviesListActivity.IMAGE_BASE_URL + list.get(i).getPosterPath())
-                .placeholder(R.drawable.load)
-                .into(myViewHolder.poster);*/
-
-        /*myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+        myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, MovieDetailsActivity.class);
-                intent.putExtra("IMAGEPATH", list.get(i).getBackPath());
-                intent.putExtra("title", list.get(i).getOriginalTitle());
-                intent.putExtra("Desc", list.get(i).getOverview());
-                intent.putExtra("ID", list.get(i).getId());
-                context.startActivity(intent);
             }
-        });*/
+        });
     }
 
     @Override
