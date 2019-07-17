db2e16bedbea91b3727d58d743a1a2e5d8dac7b6
sonpatkiut <sonpatkiut@gmail.com>
2019-07-17 19:39:12 +0530

    Update TrailersAdapter.java

--- RatingStar/app/src/main/java/com/example/icsssd358/ratingstar/TrailersAdapter.java
+++ RatingStar/app/src/main/java/com/example/icsssd358/ratingstar/TrailersAdapter.java
@@ -2,6 +2,7 @@ package com.example.icsssd358.ratingstar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -33,6 +34,10 @@ public class TrailersAdapter extends RecyclerView.Adapter<TrailersAdapter.MyView
         myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=" + list.get(i).getKey()));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("VIDEO_ID", list.get(i).getKey());
+                context.startActivity(intent);
             }
         });
     }
